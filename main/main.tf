c2924ed8f67eac599967106eac4c1cf6bf91916f
IqwinderS <Iqwinderc@gmail.com>
2023-06-27 12:53:28 -0700

    added path

--- main/main.tf
+++ main/main.tf
@@ -1,13 +1,13 @@
 # main.tf
 
 module "resource_group" {
-  source                 = ".//modules/rg"
+  source                 = "github.com/SinghIqwinder/AZ-Modules//modules/rg"
   resource_group_name    = var.resource_group_name
   location               = var.location
 }
 
 module "vnet" {
-  source                 = ".//modules/vnet"
+  source                 = "github.com/SinghIqwinder/AZ-Modules//modules/vnet"
   resource_group_name    = var.resource_group_name
   location               = var.location
   vnets                  = var.vnets
