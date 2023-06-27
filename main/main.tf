fd4e1eb94592a34a5f4e4ea2884b3cbe4c90d8a5
IqwinderS <Iqwinderc@gmail.com>
2023-06-27 12:46:09 -0700

    modified module

--- main/main.tf
+++ main/main.tf
@@ -1,13 +1,13 @@
 # main.tf
 
 module "resource_group" {
-  source                 = "./modules/resource_group"
+  source                 = ".//modules/rg"
   resource_group_name    = var.resource_group_name
   location               = var.location
 }
 
 module "vnet" {
-  source                 = "./modules/vnet"
+  source                 = ".//modules/vnet"
   resource_group_name    = var.resource_group_name
   location               = var.location
   vnets                  = var.vnets
