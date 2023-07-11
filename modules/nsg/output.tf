b76d8f78fcc9f469998a122b8e4c1da8da45e7f9
singhiqwinder <singhiqwinder40@gmail.com>
2023-07-11 11:12:19 -0400

    disabled module NSG

--- modules/nsg/output.tf
+++ modules/nsg/output.tf
@@ -1,6 +1,6 @@
-output "nsg_ids" {
-  value = {
-    for subnet in var.subnets :
-      subnet => azurerm_network_security_group.main[subnet].id
-  }
-}
+#output "nsg_ids" {
+#  value = {
+#    for subnet in var.subnets :
+#      subnet => azurerm_network_security_group.main[subnet].id
+#  }
+#}
